7491995224f4645b28b7f2785af3e963d2eefe64
katherinemli <katherine.lib.ira@gmail.com>
2024-01-29 09:47:48 -0500

    realtime animation

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="">
-        <ag-charts-vue :options="options" />
+        <ag-charts-vue :options="dynamicOption" />
     </div>
 </template>
   
 <script>
 import { AgChartsVue } from 'ag-charts-vue';
+import dataSample from './data_sample.json';
 
 export default {
     name: 'BarChart',
@@ -17,35 +18,196 @@ export default {
     },
     data() {
         return {
+            totalCountFrames: 0,
+            timer: null,
+            dataSample,
+            chLength: 0,
+            chAmount: 0,
+            colors: {
+                0: '#555555',
+                1: '#3D702E',
+                2: '#9EFDFF',
+                3: '#5dab46'
+            },
             options: {
-                // Data: Data to be displayed in the chart
-                data: [
-                    { month: "Jan", avgTemp: 2.3, iceCreamSales: 162000 },
-                    { month: "Mar", avgTemp: 6.3, iceCreamSales: 302000 },
-                    { month: "May", avgTemp: 16.2, iceCreamSales: 800000 },
-                    { month: "Jul", avgTemp: 22.8, iceCreamSales: 1254000 },
-                    { month: "Sep", avgTemp: 14.5, iceCreamSales: 950000 },
-                    { month: "Nov", avgTemp: 8.9, iceCreamSales: 200000 },
+                theme: {
+                    palette: {
+                        fills: [],
+                    },
+                    overrides: {
+                        bar: {
+                            autoSize: true,
+                            series: {
+                                stroke: "transparent",
+                                strokeWidth: 1,
+                            },
+                        },
+                    },
+                },
+                title: {
+                    text: "Frameplan",
+                    spacing: 30,
+                },
+                data:[],
+                series: [],
+                axes: [
+                    {
+                        type: "category",
+                        position: "left",
+                        line: {
+                            enabled: false,
+                        },
+                        tick: {
+                            values: ['Inroute 1', 'Inroute 2'],
+                        },
+                        gridLine: {
+                            enabled: true,
+                        },
+                    },
+                    {
+                        type: "number",
+                        position: "top",
+                        nice: false,
+                        min: 0,
+                        max: 0,
+                        label: {
+                            enabled: true,
+                        },
+                        tick: {
+                            interval: 10,
+                        },
+                        gridLine: {
+                            width: 2,
+                        },
+                    },
                 ],
-                // Series: Defines which chart type and data to use
-                series: [{ type: "bar", xKey: "month", yKey: "iceCreamSales" }],
+                legend: {
+                    enabled: false,
+                },
             },
         };
     },
     methods: {
-        initRequest(){
-            this.axios.get("/api/postreq")
-        .then(response => {
-            console.log(response);
-        })
-          .catch(function (error) {
-             console.log(error);
-        });
+        initRequest() {
+/*             this.axios.get("/api/postreq")
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                }); */
+
+            this.optionExample()
+        },
+        optionExample() {
+
+            var fillColorstInroute = [];
+            this.dataSample.reply.forEach(element => {
+                fillColorstInroute.push(this.colors[element]);
+            });
+            this.totalCountFrames = this.dataSample.reply.length;
+            var serieDynamicInroute = [];
+
+            for (let index = 0; index < this.totalCountFrames; index++) {
+                let element = {
+                    type: "bar",
+                    direction: "horizontal",
+                    xKey: "inroute",
+                    yKey: `serie${index}`,
+                    stacked: true,
+                }
+                serieDynamicInroute.push(element)
+
+            }
+            var data = [];
+            var framePlansqueletor = { inroute: 'Inroute 1' };
+            for (let index = 0; index < this.totalCountFrames; index++) {
+                framePlansqueletor[`serie${index}`] = 1;
+            }
+            data.push(framePlansqueletor);
+            this.options.theme.palette.fills = fillColorstInroute;
+            this.options.data = data;
+            console.log('this.options.data:', this.options.data)
+            this.options.series = serieDynamicInroute;
+            this.options.axes[1].max = this.totalCountFrames;
+            console.log('this.options.theme.palette.fills:', this.options.theme.palette.fills)
+            const options = {
+                theme: {
+                    palette: {
+                        fills: fillColorstInroute,
+                    },
+                    overrides: {
+                        bar: {
+                            autoSize: true,
+                            series: {
+                                stroke: "transparent",
+                                strokeWidth: 1,
+                            },
+                        },
+                    },
+                },
+                title: {
+                    text: "Frameplan",
+                    spacing: 30,
+                },
+
+                data: data,
+                series: serieDynamicInroute,
+                axes: [
+                    {
+                        type: "category",
+                        position: "left",
+                        line: {
+                            enabled: false,
+                        },
+                        tick: {
+                            values: ['Inroute 1', 'Inroute 2'],
+                        },
+                        gridLine: {
+                            enabled: true,
+                        },
+                    },
+                    {
+                        type: "number",
+                        position: "top",
+                        nice: false,
+                        min: 0,
+                        max: this.totalCountFrames,
+                        label: {
+                            enabled: true,
+                        },
+                        tick: {
+                            interval: 10,
+                        },
+                        gridLine: {
+                            width: 2,
+                        },
+                    },
+                ],
+                legend: {
+                    enabled: false,
+                },
+            };
+            return options
         },
     },
     created() {
+        
         this.initRequest();
     },
+    computed: {
+        dynamicOption(){
+            return this.optionExample();
+        }
+    },
+    mounted() {
+      this.timer = setInterval(() => {
+        this.dataSample.reply.unshift(...this.dataSample.reply.splice(-1));
+      }, 100)
+   },
+    beforeDestroy() {
+      clearInterval(this.timer)
+   }
 }
 </script>
   
