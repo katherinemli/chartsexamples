cc07b8e8f79eb064c3081beb4e3f179c159d7f41
katherinemli <katherine.lib.ira@gmail.com>
2024-01-31 14:54:16 -0500

    map iconmarker

--- src/components/MapLeaflet.vue
+++ src/components/MapLeaflet.vue
@@ -1,72 +1,133 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      Hola MAPA
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <l-map :center="center" :zoom="zoom" class="map" ref="map" @update:zoom="zoomUpdated" @update:center="centerUpdated">
-      <l-tile-layer :url="url">
-      </l-tile-layer>
+  <div style="height: 1000px; width: 100%">
+    <l-map v-if="showMap" :zoom="zoom" :center="center" :options="mapOptions" style="height: 80%"
+      @update:center="centerUpdate" @update:zoom="zoomUpdate">
+      <l-tile-layer :url="url" :attribution="attribution" />
+      <l-marker v-for="marker in markers" :key="marker.id + '-mark'" :lat-lng="latLng(marker.lat, marker.long)">
+        <!-- <l-tooltip :options="{ permanent: true, interactive: true }"> -->
+        <l-tooltip :options="{ permanent: false, interactive: true }">
+          <div @click="innerClick">
+            {{ marker.location }}
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-tooltip>
+        <l-icon :icon-url="customMarker"></l-icon>
+        <l-popup>
+          <div @click="innerClick">
+            I am a popup {{ marker }}
+            {{ marker.lat }}
+            {{ marker.long }}
+            <p v-show="showParagraph">
+
+            </p>
+          </div>
+        </l-popup>
+      </l-marker>
+<!--       <l-marker :lat-lng="withTooltip">
+        <l-icon :icon-url="customMarker"></l-icon>
+        <l-tooltip :options="{ permanent: true, interactive: true }">
+          <div @click="innerClick">
+            I am a tooltip
+            <p v-show="showParagraph">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+              Donec finibus semper metus id malesuada.
+            </p>
+          </div>
+        </l-tooltip>
+      </l-marker> -->
     </l-map>
   </div>
 </template>
-
+  
 <script>
-import { LMap, LTileLayer } from 'vue2-leaflet';
+import L from 'leaflet';
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip, LIcon } from "vue2-leaflet";
 import 'leaflet/dist/leaflet.css';
-
+import customMarker from '../assets/iconsLocation.png';
+import {
+  Icon
+} from 'leaflet';
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 export default {
-  name: 'MapLeaflet',
-  props: {
-    msg: String
-  },
+  name: "MapLeaflet",
   components: {
     LMap,
-    LTileLayer
+    LTileLayer,
+    LMarker,
+    LPopup,
+    LTooltip,
+    LIcon
   },
   data() {
     return {
+      customMarker,
+      zoom: 13,
+      center: latLng(40.724634757833414, -74.00701717096757),
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      center: [49.1193089, 6.1757156],
-      zoom: 12,
-    }
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withPopup: latLng(47.41322, -1.219482),
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 11.5,
+      currentCenter: latLng(40.724634757833414, -74.00701717096757),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: false,
+      markers: []
+    };
   },
   methods: {
-    zoomUpdated(zoom) {
-      this.zoom = zoom;
-      console.log(this.markers)
+    latLng: function (lat, lng) {
+      console.log('latLng:', L.latLng(lat, lng))
+      return L.latLng(lat, lng);
     },
-    centerUpdated(center) {
-      this.center = center;
+    initRequest() {
+      this.axios.get("/api/points")
+        .then(response => {
+          console.log(response.data);
+          this.markers = response.data;
+          this.showMap = true;
+          //this.markers = this.markers.slice(2, 3)
+
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+
+    },
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
     }
-  }
-}
+  },
+  created() {
+    this.initRequest();
+
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-.map {
-   position: absolute;
-   width: 100%;
-   height: 100%;
-   overflow :hidden
- }
-</style>
+  
+<style></style>
+  
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+//axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.baseURL = 'https://golandbackend-09fdf720d244.herokuapp.com/api'
+// this part resolve an issue where the markers would not appear
 
 
-Vue.use(VueAxios, axios)
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
